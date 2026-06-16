3ae0c387cb7692fc5fa1a565d3140803b73d0ebc
Atifa Sajida <satifa022006@gmail.com>
2026-06-16 23:40:49 +0530

    chore: configure Flask-Login authentication

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from app.config import Config
-
 from app.extensions import (
     db,
     bcrypt,
@@ -20,21 +19,26 @@ def create_app():
 
     app.config.from_object(Config)
 
-
-    from app.auth import auth_bp
-    from app.diary import diary_bp
-    from app.profile import profile_bp
-    
-    
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(diary_bp)
-    app.register_blueprint(profile_bp)
-
-
-
+    # Initialize Extensions
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
+
+    # Flask-Login Configuration
+    login_manager.login_view = "auth.login"
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.get(User, int(user_id))
+
+    # Register Blueprints
+    from app.auth import auth_bp
+    from app.diary import diary_bp
+    from app.profile import profile_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(diary_bp)
+    app.register_blueprint(profile_bp)
 
     return app
