60759f70b01a1dc7b2cda0d91defa4e9c4fa4566
Atifa Sajida <satifa022006@gmail.com>
2026-06-18 19:00:20 +0530

    feat: add diary entry creation route

--- app/diary/routes.py
+++ app/diary/routes.py
@@ -1,7 +1,36 @@
 from app.diary import diary_bp
+from flask import render_template, request, redirect, url_for
+
+from flask_login import (
+    login_required,
+    current_user
+)
+
+from app.models import Entry
+from app.extensions import db
 
 
+@diary_bp.route("/new", methods=["GET", "POST"])
+@login_required
+def new_entry():
 
-@diary_bp.route("/test")
-def test():
-  return "Diary Blueprint Working"
+    if request.method == "GET":
+        return render_template(
+            "diary/new_entry.html"
+        )
+
+    title = request.form.get("title")
+    content = request.form.get("content")
+
+    entry = Entry(
+        title=title,
+        content=content,
+        user_id=current_user.id
+    )
+
+    db.session.add(entry)
+    db.session.commit()
+
+    return redirect(
+        url_for("diary.dashboard")
+    )
