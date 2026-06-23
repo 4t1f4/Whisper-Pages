d0edcf4300b865c0dbffc2551c3da734e745061c
Atifa Sajida <satifa022006@gmail.com>
2026-06-23 22:14:17 +0530

    feat: add Fernet encryption utilities

---
+++ app/utils/crypto.py
@@ -0,0 +1,13 @@
+from cryptography.fernet import Fernet
+import os
+
+cipher = Fernet(os.getenv("ENCRYPTION_KEY").encode())
+
+
+def encrypt_text(text):
+  return cipher.encrypt(text.encode().decode())
+
+
+
+def decrypt_text(text):
+  return cipher.decrypt(text.encode().decode())
